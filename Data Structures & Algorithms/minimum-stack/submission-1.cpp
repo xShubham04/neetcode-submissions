04baca8790b7ca3f4af705e60a782f79db0b98cb
Shubham Kumar <hamadashubham@gmail.com>
2026-06-28 18:02:28 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.cpp
@@ -0,0 +1,25 @@
+class MinStack {
+public:
+stack<int> s,st;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        s.push(val);
+        if(st.empty() || val<=st.top()) st.push(val);
+    }
+    
+    void pop() {
+        if(s.top()==st.top()) st.pop();
+        s.pop();
+    }
+    
+    int top() {
+        return s.empty() ? -1 : s.top();
+    }
+    
+    int getMin() {
+        return st.empty() ? -1 : st.top();
+    }
+};
