43c2e7870a68c6dd2296c455546661c57b7465eb
Shubham Kumar <hamadashubham@gmail.com>
2026-07-16 22:49:15 +0530

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.cpp
@@ -0,0 +1,18 @@
+class Solution {
+   public:
+    bool isValid(string s) {
+        int index = 0;
+        stack<int> st;
+        while (index < s.size()) {
+            if (s[index] == '(' || s[index] == '{' || s[index] == '[')
+                st.push(s[index]);
+            else if ((s[index] == ')' && !st.empty() && st.top() == '(') ||
+                     (s[index] == ']' && !st.empty() && st.top() == '[') ||
+                     (s[index] == '}' && !st.empty() && st.top() == '{'))
+                     st.pop();
+            else return false;         
+                     index++;
+        }
+        return st.empty();
+    }
+};
