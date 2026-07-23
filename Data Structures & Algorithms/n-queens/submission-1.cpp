4e7e7a4c3d454182d7c1db9f41875bc9af0658b9
Shubham Kumar <hamadashubham@gmail.com>
2026-07-23 23:56:29 +0530

    Add: n-queens - submission-1

---
+++ Data Structures & Algorithms/n-queens/submission-1.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+bool isSafe(int row,int col,vector<string>& board,int n){
+    int x=row;
+    int y=col;
+    while(y>=0){
+        if(board[x][y]=='Q') return false;
+        y--;
+    }
+    x=row;
+    y=col;
+    while(x>=0 && y>=0){
+        if(board[x][y]=='Q') return false;
+        x--;
+        y--;
+    }
+    x=row;
+    y=col;
+    while(y>=0 && x<n){
+        if(board[x][y]=='Q') return false;
+        x++;
+        y--;
+    }
+    return true;
+}
+void solve(int col,vector<string>& board,vector<vector<string>>& ans,int n){
+    if(col==n){
+        ans.push_back(board);
+        return;
+    }
+    for(int row=0;row<n;row++){
+        if(isSafe(row,col,board,n)){
+        board[row][col]='Q';
+        solve(col+1,board,ans,n);
+        board[row][col]='.';
+        }
+    }
+}
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> res;
+        vector<string> board(n,string(n,'.'));
+        solve(0,board,res,n);
+        return res;
+    }
+};
