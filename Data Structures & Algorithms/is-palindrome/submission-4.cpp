0dbeb3a0ad5c62791cba01650d51338e74d8d392
Shubham Kumar <hamadashubham@gmail.com>
2026-07-25 00:20:48 +0530

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string temp;
+        for(char c:s){
+            if(isalnum(c))
+            temp+=tolower(c);
+        }
+        int left=0,right=temp.size()-1;
+        while(left<right){
+            if(temp[left]!=temp[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
