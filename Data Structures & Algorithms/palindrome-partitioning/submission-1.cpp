825814770437eb551e65e1ad7bf2c3c02e28111d
Shubham Kumar <hamadashubham@gmail.com>
2026-07-28 23:42:53 +0530

    Add: palindrome-partitioning - submission-1

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+bool isPalindrome(string s,int start,int end){
+    while(start<=end){
+        if(s[start]!=s[end]) return false;
+        start++;
+        end--;
+    }
+    return true;
+}
+void backtrack(int index,string s,vector<string>& curr,vector<vector<string>>& res){
+    if(index==s.size()){
+        res.push_back(curr);
+        return;
+    }
+    for(int i=index;i<s.size();i++){
+        if(isPalindrome(s,index,i)){
+            curr.push_back(s.substr(index,i-index+1));
+            backtrack(i+1,s,curr,res);
+            curr.pop_back();
+        }
+    }
+}
+    vector<vector<string>> partition(string s) {
+        vector<string> curr;
+        vector<vector<string>> res;
+        backtrack(0,s,curr,res);
+        return res;
+    }
+};
