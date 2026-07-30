a50b484891d44af2a4eaab9b62cec951313cf445
Shubham Kumar <hamadashubham@gmail.com>
2026-07-30 13:56:05 +0530

    Add: decode-ways - submission-2

---
+++ Data Structures & Algorithms/decode-ways/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+int backtrack(int index,string s,vector<int>& dp){
+    if(index==s.size()) return 1;
+    if(s[index]=='0') return 0;
+    if(dp[index]!=-1) return dp[index];
+    int res=backtrack(index+1,s,dp);
+    if(index<s.size()-1){
+        if(s[index]=='1' || s[index]=='2' && s[index+1]<'7')
+        res+=backtrack(index+2,s,dp);
+    }
+    return dp[index]=res;
+}
+    int numDecodings(string s) {
+        vector<int> dp(s.size(),-1);
+        return backtrack(0,s,dp);
+    }
+};
