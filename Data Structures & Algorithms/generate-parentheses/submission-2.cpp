638b5009f4a036e6edcaf4020e67f6a41881ecb0
Shubham Kumar <hamadashubham@gmail.com>
2026-08-09 21:47:10 +0530

    Add: generate-parentheses - submission-2

---
+++ Data Structures & Algorithms/generate-parentheses/submission-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+void solve(int open,int close,int n,string curr,vector<string>& res){
+    if(curr.size()==2*n){
+        res.push_back(curr);
+        return;
+    }
+    if(open<n) 
+    solve(open+1,close,n,curr+'(',res);
+    if(close<open)
+    solve(open,close+1,n,curr+')',res);
+}
+    vector<string> generateParenthesis(int n) {
+        string curr;
+        vector<string> res;
+        solve(0,0,n,curr,res);
+        return res;
+    }
+};
