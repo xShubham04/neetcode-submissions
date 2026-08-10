20ae043fdbaf995761973845d853ae7702e7f9b4
Shubham Kumar <hamadashubham@gmail.com>
2026-08-10 18:11:26 +0530

    Add: combinations-of-a-phone-number - submission-2

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+void solve(int index,string digits,unordered_map<char,list<char>>& map,string curr,vector<string>& res){
+    if(index==digits.size()){
+        res.push_back(curr);
+        return;
+    }
+    for(char ch:map[digits[index]])
+    solve(index+1,digits,map,curr+ch,res);
+    
+}
+    vector<string> letterCombinations(string digits) {
+        if(digits.empty()) return {};
+        unordered_map<char,list<char>> map;
+        vector<string> res;
+        map['2']={'a','b','c'};
+        map['3']={'d','e','f'};
+        map['4']={'g','h','i'};
+        map['5']={'j','k','l'};
+        map['6']={'m','n','o'};
+        map['7']={'p','q','r','s'};
+        map['8']={'t','u','v'};
+        map['9']={'w','x','y','z'};
+        solve(0,digits,map,"",res);
+        return res;
+    }
+};
