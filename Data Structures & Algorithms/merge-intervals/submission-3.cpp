c9a20af58811778bb8bd8933ba2e6314648f6da9
Shubham Kumar <hamadashubham@gmail.com>
2026-08-13 15:59:37 +0530

    Add: merge-intervals - submission-3

---
+++ Data Structures & Algorithms/merge-intervals/submission-3.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> res;
+        sort(intervals.begin(),intervals.end());
+        for(auto&p: intervals){
+            if(res.empty() || res.back()[1]<p[0])
+            res.push_back(p);
+            else
+            res.back()[1]=max(res.back()[1],p[1]);
+        }
+        return res;
+    }
+};
